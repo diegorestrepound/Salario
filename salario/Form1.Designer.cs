0f1a7d529ea3a172545de05ac2cd6e1a2081018b
Diego Restrepo <diegorestrepound@gmail.com>
2019-09-15 12:04:53 -0500

    este programa calcula el salario de una persona y respecto a este le incrementa o le descuenta su pago final

---
+++ salario/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace salario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txthorastrabajadas = new System.Windows.Forms.TextBox();
+            this.txtvalorhora = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtsalariofinal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbldescuento = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Horas trabajadas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor por hora:";
+            // 
+            // txthorastrabajadas
+            // 
+            this.txthorastrabajadas.Location = new System.Drawing.Point(292, 84);
+            this.txthorastrabajadas.Name = "txthorastrabajadas";
+            this.txthorastrabajadas.Size = new System.Drawing.Size(100, 20);
+            this.txthorastrabajadas.TabIndex = 2;
+            // 
+            // txtvalorhora
+            // 
+            this.txtvalorhora.Location = new System.Drawing.Point(292, 143);
+            this.txtvalorhora.Name = "txtvalorhora";
+            this.txtvalorhora.Size = new System.Drawing.Size(100, 20);
+            this.txtvalorhora.TabIndex = 3;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(248, 199);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 4;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtsalariofinal
+            // 
+            this.txtsalariofinal.Location = new System.Drawing.Point(400, 284);
+            this.txtsalariofinal.Name = "txtsalariofinal";
+            this.txtsalariofinal.Size = new System.Drawing.Size(100, 20);
+            this.txtsalariofinal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Salario final:";
+            // 
+            // lbldescuento
+            // 
+            this.lbldescuento.AutoSize = true;
+            this.lbldescuento.Location = new System.Drawing.Point(400, 236);
+            this.lbldescuento.Name = "lbldescuento";
+            this.lbldescuento.Size = new System.Drawing.Size(0, 13);
+            this.lbldescuento.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbldescuento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtsalariofinal);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtvalorhora);
+            this.Controls.Add(this.txthorastrabajadas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txthorastrabajadas;
+        private System.Windows.Forms.TextBox txtvalorhora;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtsalariofinal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbldescuento;
+    }
+}
+
---
+++ salario/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double hT, vH;
+            hT = double.Parse(txthorastrabajadas.Text);
+            vH = double.Parse(txtvalorhora.Text);
+           
+            double resultado, salarioFinal;
+            resultado = vH * hT;
+            if (resultado < 700000)
+            {
+                salarioFinal = resultado-(resultado * 0.10);
+                txtsalariofinal.Text = salarioFinal.ToString();
+                lbldescuento.Text = "incremento del 10%";
+            }
+            else if (resultado < 1000000)
+            {
+                salarioFinal = resultado-(resultado*0.2);
+                txtsalariofinal.Text = salarioFinal.ToString();
+                lbldescuento.Text = "descuento del 2%";
+            }
+            else
+            {
+                salarioFinal = resultado-(resultado*0.3);
+                txtsalariofinal.Text = salarioFinal.ToString();
+                lbldescuento.Text = "descuento de 5%  //  incremento de 2%";
+            }
+        }
+    }
+}
